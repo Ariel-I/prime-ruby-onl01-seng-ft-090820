cab2bd1f51dc96f405b5f8e48ee42bb31e302c44
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-14 20:44:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(integer)
   if integer <= 1
     return false 
-    elsif (2).each do |i|
+    elsif (2..integer -1).each do |i|
       if integer % i == 0
         return false 
       end
